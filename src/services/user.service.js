81b7f7ede4d6845d5487abd00927f0f8fcb92f28
psalmuelle <bjoestores@gmail.com>
2022-09-20 13:33:09 +0100

    finish api call

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -11,16 +11,165 @@ const client = axios.create({
     return (
         client
       .post("/note/create/", {
-        
-
-      }).then((response)=>{
+       title : data.title,
+       note : data.note
+      },
+      {
+        headers: authHeader()
+      })
+      .then((response)=>{
         console.log(response.data)
-        
-        response.data?.user_info?.token && localStorage.setItem("user", JSON.stringify(response.data))
-
       }).catch(err =>{
         console.log(err)
         alert(err)
       })
     )
+}
+
+
+const getNote=()=>{
+    return (
+        client
+      .get("/note/get/", {
+        headers: authHeader()
+      })
+      .then((response)=>{
+        console.log(response.data)
+      }).catch(err =>{
+        console.log(err)
+        alert(err)
+      })
+    )
+}
+
+const createPlan=(data)=>{
+    return (
+        client
+      .post("/plan/create/", {
+       plan : data.plan
+      },
+      {
+        headers: authHeader()
+      })
+      .then((response)=>{
+        console.log(response.data)
+      }).catch(err =>{
+        console.log(err)
+        alert(err)
+      })
+    )
+}
+
+const getPlan=()=>{
+    return (
+        client
+      .get("/plan/get/", {
+        headers: authHeader()
+      })
+      .then((response)=>{
+        console.log(response.data)
+      }).catch(err =>{
+        console.log(err)
+        alert(err)
+      })
+    )
+}
+
+
+const deletePlan=(id)=>{
+    return (
+        client
+        .delete(`/plan/delete/${id}`)
+    )
+}
+
+
+const createTodo=(data)=>{
+    return (
+        client
+      .post("/todo/create/", {
+       title: data.title,
+       description: data.description,
+       deadline: data.deadline
+      },
+      {
+        headers: authHeader()
+      })
+      .then((response)=>{
+        console.log(response.data)
+      }).catch(err =>{
+        console.log(err)
+        alert(err)
+      })
+    )
+}
+
+const getTodo=()=>{
+    return (
+        client
+      .get("/todo/get/", {
+        headers: authHeader()
+      })
+      .then((response)=>{
+        console.log(response.data)
+      }).catch(err =>{
+        console.log(err)
+        alert(err)
+      })
+    )
+}
+
+const createTodoList=(data, todoId)=>{
+    return (
+        client
+      .post("/todolist/create/", {
+       list: data.list,
+       todoid: todoId
+      })
+      .then((response)=>{
+        console.log(response.data)
+      }).catch(err =>{
+        console.log(err)
+        alert(err)
+      })
+    )
+}
+
+const getTodoList=(id)=>{
+    return (
+        client
+      .get(`/todolist/get/${id}/`)
+      .then((response)=>{
+        console.log(response.data)
+      }).catch(err =>{
+        console.log(err)
+        alert(err)
+      })
+    )
+}
+
+const updateTodoList=(data, id)=>{
+    return (
+      client
+      .put(`/todolist/update/${id}/`,{
+        completed: data.completed
+      })
+      .then((res)=>{
+        console.log(res)
+      })
+    )
+}
+
+
+export default {
+  createTodo,
+  createNote,
+  createPlan,
+  createTodoList,
+  getNote,
+  getPlan,
+  getTodo,
+  getTodoList,
+  deletePlan,
+  updateTodoList
 }
